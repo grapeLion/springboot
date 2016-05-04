dbac1fcf83775c761279920ab4421af71cecfcc1
weizheng <msxf@com>
2016-05-04 12:29:04 +0800

    基础架构

---
+++ src/main/java/cn/ms/springboot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package cn.ms.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by weizheng on 2016/5/4.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello,world";
+    }
+}
---
+++ src/main/java/cn/ms/springboot/domain/Person.java
@@ -0,0 +1,7 @@
+package cn.ms.springboot.domain;
+
+/**
+ * Created by weizheng on 2016/5/4.
+ */
+public class Person {
+}
